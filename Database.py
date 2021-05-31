1114bfb138c5f6f850184448c21d46deeadb71ef
Jacob Bulloch <64548773+jpbulloch5@users.noreply.github.com>
2021-05-31 15:31:18 -0600

    update

--- Database.py
+++ Database.py
@@ -2,58 +2,47 @@ class Table:
     '''
     A table is a list of records.  A Table has a set of categories that are valid for
     the fields in each record.
+        record<dict> = {primary_key<any unique>: fields<dict>}
+        field<dict> = {category<any unique>: value<any>}
     '''
-    def __init__(self, categories: tuple, records: list, primary_key_type: type = int) -> None:
-        super(self)
-        self.__primary_key_type = primary_key_type
+    def __init__(self, categories: tuple, *records: dict) -> None:
+        
+        self.__categories = categories
         self.__primary_key_set: set = set()
-        self.__records: list = []
-        for record in records:
-            if record.get_primary_key() not in self.__primary_key_set:
-                self.__primary_key_set.add(record.get_primary_key())
-                self.__records.append(record)
-            
+        self.__records: dict = dict()
+
+        record_primary_keys: int = records.keys()
+        record_fields: dict = records.values()
+
+        field_categories: str = record_fields.keys()    # alias = records.values().keys()
+        field_values = record_fields.values()           # alias = records.values().values()
+        
+        for record_primary_key, record_field in records:
+            if (record_primary_key not in self.__primary_key_set    # if the record has a unique primary key
+            and record_field.keys() in self.__categories):          # and the field category is also a table category
+                
+
         self.__categories: set = categories
+    # END __init__()
 
     def __str__(self):
         
-        # store the formatted heading line because we will need it's lenght to generate a separater line
-        heading_string = '\t'.join(self.__categories)
+        # store the formatted heading line because we will need it's length to generate a separater line
+        heading = '\t'.join(self.__categories)
 
         # start a new line and output the heading as a row of text with a row of '=' characters to
-        # separate the heading from the records in the table
-        output_string = f'\n{heading_string}\n{"=" * len(heading_string)}'
+        # separate the heading from the records in the table, then add each new record as a new line, 
+        # plus an extra blank line at the end
+        return f'\n{heading}\n{"=" * len(heading)}' + f'{"\n".join(self.__records)}\n'
+    # END __str__()
 
-        # add each new record as a new line, plus an extra blank line at the end
-        output_string += f'{"\n".join(self.__records)}\n'
-        
-        return output_string
-    
     def get_categories(self):
         return self.__categories
 
-    def add_record(self, new_record):
+    def add_record(self, primary_key, fields: dict):
         pass
 
-class Record:
-    '''
-    A record is a row of fields in a table that is uniquely identified by 
-    some primary key.
-    '''
-    def __init__(self, associated_table: Table, primary_key, fields: dict) -> None:
-        self.__primary_key = primary_key
-        self.__fields = fields
-        self.__associated_table = associated_table
-    
-    def all_fields_valid(self) -> bool:
-        for category in self.__fields.keys():
-            if category not in self.__associated_table.get_headings():
-                return False
-        return True
-    
-    def get_primary_key(self):
-        return self.__primary_key
+    def record_is_valid(self, record_to_check) -> bool:
+        pass
 
-    def get_fields(self):
-        return self.__fields
         
