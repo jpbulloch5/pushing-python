6dcba531f75347a506b4ca85abed3b5e1e6aad56
Jacob Bulloch <64548773+jpbulloch5@users.noreply.github.com>
2021-06-01 13:53:27 -0600

    updates to table.py

--- Table.py
+++ Table.py
@@ -1,26 +1,35 @@
-import datetime
+
+
 class Table:
     '''
     A table is a list of records.  A Table has a set of categories that are valid for
     the fields in each record.
-        records<dict> = {primary_key<any unique>: fields<dict>}
-        fields<dict> = {category<any in table-level category set>: data<any>}
+        records<dict> = {primary_key<any>: fields<dict>}
+        fields<dict> = {category_1<any>: data_1<any>, category_2<any>: data_2<any>}
+            ***categories within the fields MUST correspond with a category in the Table
     '''
-    def __init__(self, categories: set = None, *records: dict) -> None:
+    
+
+    def __init__(self, *records: dict, categories: set = None) -> None:
+        FIRST_RECORD = 0
+
         self.__primary_key_set: set = set()
+        self.__categories: set = set()
         self.__records: dict = dict()
 
         # if table categories were explicitly provided...
         if categories:
 
             # assign them
-            self.__categories: set = categories
+            self.__categories = categories
 
         # otherwise, check if at least one record was provided...
         elif records:
 
+            self.__categories = set(category for category in [field for field in records[FIRST_RECORD].values()][0].keys())
+   
             # and assign the categories using the first record as a template
-            self.__categories: set = set(field_category for field_category in records[0].values()[0])
+            #self.__categories = set(field_category for field_category in records[0].values())
             
         # otherwise...
         else:
@@ -28,19 +37,29 @@ class Table:
             # raise a syntax error and teach the user how to call the constructor correctly
             raise SyntaxError('ERROR: If no records are provided for the Table object constructor call, then table categories (columns) must be provided explicitly.')
 
-        self.add_records(records)
+        self.__records = records
 
     # END __init__()
 
     def __str__(self):
         
         # store the formatted heading line because we will need it's length to generate a separater line
-        heading = '\t'.join(self.__categories)
+        heading = '#\t'
+        for field in self.__categories:
+            heading += str(field)+ '\t'
+
+        body = '\n'
+        print(f'line 50: {self.__records}')
+        for primary_key, fields in self.__records[0].items():
+            body += f'{primary_key}\t'
+            for data in fields.values():
+                body += f'{data}\t'
+            body += '\n'
 
         # start a new line and output the heading as a row of text with a row of '=' characters to
         # separate the heading from the records in the table, then add each new record as a new line, 
         # plus an extra blank line at the end
-        return f'/n{heading}/n{"=" * len(heading)}{"/n".join(self.__records)} /n'
+        return f'\n{heading}\n{"=" * len(heading)}\n{body}\n'
     # END __str__()
 
     def get_categories(self):
@@ -71,9 +90,11 @@ class Table:
 #Testing code:
 #######################################################
 if __name__ == '__main__':
+    import datetime
 
-    record_example: dict = {1, (('Last Name', 'Doe'), ('First Name', 'John'), ('Major', 'Computer Science'), ('Date of Hire', datetime.date(2021,1,12)), ('Salary', 45_000))}
+    record_example: dict = {1: {'Last Name': 'Doe', 'First Name': 'John', 'Major': 'Computer Science', 'Date of Hire': datetime.date(2021,1,12), 'Salary': 45_000}}
     table_example = Table(record_example)
+    print(table_example)
 
     '''
     #Some stuff the verify how any() and all() work
